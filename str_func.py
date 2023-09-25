f2082d59de11a0efb2d901829be507e1a7c68763
Mihkel Metsa <mihkel992@gmail.com>
2023-09-25 10:07:01 -0700

    added new func capitalize for words lalala

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def upper_string(word):
     return word.upper()
+
+
+def capital_words(string) -> str:
+    """return all words from list for main letter"""
+    return ' 'join(s.capitalize() for s in string.split())
+
